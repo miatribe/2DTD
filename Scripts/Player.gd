d1f331d265c02063ddb28b3d82afa24392d233cb
Michael Wheatley <miatribe@users.noreply.github.com>
2024-10-03 20:46:03 -0400

    Lives
    Added lives.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,13 +9,16 @@ var kills:int = 0:
 	set(kills_in):
 		kills = kills_in
 		ui.update_kills(kills)
-
+var lives:int = 5:
+	set(lives_in):
+		lives = lives_in
+		ui.update_lives(lives)
 @onready var ui: Node2D = $"../UI"
 
 
 func _ready() -> void:
 	EventBus.CreepDied.connect(creep_died)
-
+	EventBus.TargetReached.connect(player_damaged)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click") && gold >= td.tower_cost:
@@ -30,8 +33,15 @@ func _input(event: InputEvent) -> void:
 		print("debug_damage_creeps")
 		for creeper in td.creepers.get_children():
 			creeper.take_damage(2)
+	if event.is_action_pressed("debug_damage_player"):
+		print("debug_damage_player")
+		lives -= 1
 
 
 func creep_died(gold_in:int) -> void:
 	gold +=gold_in
 	kills +=1
+
+
+func player_damaged() -> void:
+	lives -= 1
